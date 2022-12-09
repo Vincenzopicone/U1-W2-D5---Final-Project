e14d304911cbdc250f9c8291079a79b4a1e0d62d
Vincenzo Picone <vincenzopicone88@icloud.com>
2022-12-09 14:31:22 +0100

    HTML CSS BETA e D5 Da finire

--- D5.js
+++ D5.js
@@ -32,6 +32,14 @@ console.log("esercizio 3:", reversed);
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
 
+let first = pets.shift();
+let last = pets.pop();
+
+pets.push(first);
+pets.unshift(last);
+
+console.log("Esercizio 4:", pets);
+
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
 */
@@ -56,20 +64,55 @@ const cars = [
   },
 ];
 
+for (let index = 0; index < cars.length; index++) {
+  cars[index].licensePlate = "B+B1";
+}
+
+console.log("Esercizio 5:", cars);
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
+
+const newcar = {
+  brand: "Ford",
+  model: "Kuga",
+  color: "grey",
+  trims: ["titanium", "st-linex", "sport"],
+  licensePlate: "B+B1",
+};
+
+cars.push(newcar);
+
+for (let index = 0; index < cars.length; index++) {
+  cars[index].trims.pop();
+}
+
+console.log("Esercizio 6:", cars);
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
 const justTrims = [];
 
+for (let index = 0; index < cars.length; index++) {
+  justTrims.push(cars[index].trims[0]);
+}
+
+console.log("Esercizio 7:", justTrims);
+
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+
+for (let index = 0; index < cars.length; index++) {
+  if (cars[index].color.charAt(0) === "b") {
+    console.log("Esercizio 8: FIZZ");
+  } else {
+    console.log("Esercizio 8: BUZZ");
+  }
+}
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
@@ -77,6 +120,16 @@ const justTrims = [];
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ];
+
+let cicloIndex = 0;
+
+while (cicloIndex < numericArray.lenght) {
+  console.log("Esercizio 9:", numericArray[cicloIndex]);
+  if (numericArray[cicloIndex] === 32) {
+    break;
+  }
+  cicloIndex++;
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
